df5cb02b7944a7dc2135f5bc076cf25dc56287e9
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-03-09 17:51:27 -0400

    Implemented crud operations without schema validation

--- index.js
+++ index.js
@@ -19,3 +19,30 @@ server.post('/api/users', (req, res) => {
         newUser: newUser
     })
 })
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users.find(item => item.id = id);
+    res.status(200).json({
+        message: `Got user with id: ${id}.`,
+        user: user
+    })
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedUser = req.body;
+    users = users.map(item => item.id === id ? updatedUser : item);
+    res.status(201).json({
+        message: `Updated user with id: ${id}.`,
+        updatedUser: updatedUser
+    })
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    users = users.filter(item => item.id !== id);
+    res.status(202).json({
+        message: `Deleted user with id: ${id}.`
+    })
+})
